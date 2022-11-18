14ce3b177774b24ee74114b64794e47997f48630
SantiagoLira <120036630@upq.edu.mx>
2022-11-18 16:01:25 -0600

    Creacion de menu

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -10,19 +10,25 @@
     <!--  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <!-- Fuentes para texto -->
+    <link href='//fonts.googleapis.com/css?family=Signika+Negative:300' rel='stylesheet' type='text/css'>
+    <!-- Script de animacion -->
+    <script src="{{ asset('js/animacion_menu.js') }}"></script>
     <title>Estancia_2</title>
 </head>
 
 <body>
-    <p>
-
-    <div class="typewriter">
-        <h1>Let's Start!</h1> 
-        <br>
-        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+    <h1>English Game</h1>
+    <div class="wrapper">
+        <div >
+            <ul class="nav">
+                <li class="btn">new game</li>
+                <li class="btn">options</li>
+                <li class="btn">credits</li>
+            </ul>
+        </div>
     </div>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
 </body>
 
